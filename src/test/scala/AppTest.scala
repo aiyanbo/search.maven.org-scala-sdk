f002fdeaf040ec47053d1b9fe40a0df6979bca78
Andy Ai <yanbo.ai@gmail.com>
2015-09-18 00:02:49 +0800

    Init commit

--- src/test/scala/AppTest.scala
+++ src/test/scala/AppTest.scala
@@ -1,7 +1,23 @@
+import java.util.concurrent.Future
+
+import com.ning.http.client.{ AsyncHttpClient, Response }
+import org.jmotor.tools.MavenSearchRequest
 import org.scalatest._
 
-class AppTest extends FlatSpec with Matchers{
-  "Default test" should "Print something" in {
-    println("I'm a scala test")
+class AppTest extends FunSuite {
+  test("List stats") {
+    val rootPath: String = "http://search.maven.org/solrsearch/select"
+    val asyncHttpClient: AsyncHttpClient = new AsyncHttpClient()
+    val request = MavenSearchRequest(Some("org.scala-lang"), Some("scala-reflect"), None)
+    val f: Future[Response] = asyncHttpClient.prepareGet(s"$rootPath?${request.toParameter}").execute()
+    val r = f.get()
+    if (r.getStatusCode == 200) {
+      println(r.getResponseBody)
+    }
+  }
+
+  test("To parameters") {
+    val request = MavenSearchRequest(Some("org.scala-lang"), Some("scala-reflect"), None)
+    println(request.toParameter)
   }
 }
