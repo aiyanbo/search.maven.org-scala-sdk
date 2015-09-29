64a9c225ea43d065551f58e3a50e3c5c173be7c3
Andy Ai <yanbo.ai@gmail.com>
2015-09-29 17:07:19 +0800

    Get latest version

--- src/main/scala/org/jmotor/tools/MavenSearchClient.scala
+++ src/main/scala/org/jmotor/tools/MavenSearchClient.scala
@@ -1,12 +1,12 @@
 package org.jmotor.tools
 
-import java.util.concurrent.{ Future, TimeUnit }
+import java.util.concurrent.{Future, TimeUnit}
 
-import com.fasterxml.jackson.databind.{ DeserializationFeature, ObjectMapper }
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
-import com.ning.http.client.{ AsyncHttpClient, Response }
-import org.jmotor.tools.dto.{ MavenSearchRequest, Artifact }
+import com.ning.http.client.{AsyncHttpClient, Response}
+import org.jmotor.tools.dto.{MavenSearchRequest, Artifact}
 
 /**
  * Component:
@@ -23,14 +23,26 @@ object MavenSearchClient {
   mapper.registerModule(DefaultScalaModule)
   mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
-  def search(request: MavenSearchRequest): List[Artifact] = {
+  def search(request: MavenSearchRequest)(implicit timeout: Int = 5000): List[Artifact] = {
     val f: Future[Response] = httpClient.prepareGet(s"$rootPath?${request.toParameter}").execute()
-    val response = f.get
+    val response = f.get(timeout, TimeUnit.MILLISECONDS)
     if (response.getStatusCode == 200) {
       val docs = for (m ← """.*"docs" ?: ?(\[.*\]).*""".r findFirstMatchIn response.getResponseBody) yield m group 1
       mapper.readValue[List[Artifact]](docs.getOrElse("[]"))
     } else {
       List.empty[Artifact]
+    }
+  }
+
+  def latestVersion(groupId: String, artifactId: String)(implicit timeout: Int = 5000): String = {
+    val request = MavenSearchRequest(Some(groupId), Some(artifactId), None, core = "ga", rows = 1)
+    val f: Future[Response] = httpClient.prepareGet(s"$rootPath?${request.toParameter}").execute()
+    val response = f.get(timeout, TimeUnit.MILLISECONDS)
+    if (response.getStatusCode == 200) {
+      val latest = for (m <- """"latestVersion": ?"([\d|\w|.|-]*)"""".r findFirstMatchIn response.getResponseBody) yield m group 1
+      latest.orNull
+    } else {
+      null
     }
   }
 }
--- src/test/scala/AppTest.scala
+++ src/test/scala/AppTest.scala
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import com.ning.http.client.{AsyncHttpClient, Response}
+import org.jmotor.tools.MavenSearchClient
 import org.jmotor.tools.dto.{MavenSearchRequest, Artifact}
 import org.scalatest._
 
 class AppTest extends FunSuite {
   test("JSON") {
     val start = System.currentTimeMillis()
-    val request = MavenSearchRequest(Some("org.scala-lang"), Some("scala-reflect"), None, wt = "json")
+    val request = MavenSearchRequest(Some("org.scala-lang"), Some("scala-reflect"), None, wt = "json", core = "ga", rows = 50)
     val s = execute(request)
     val docsExpr = """.*"docs" ?: ?(\[.*\]).*""".r
     val mapper = new ObjectMapper() with ScalaObjectMapper
@@ -19,6 +20,12 @@ class AppTest extends FunSuite {
     val docs = for (m ← docsExpr findFirstMatchIn s) yield m group 1
     val results = mapper.readValue[List[Artifact]](docs.getOrElse("[]"))
     println(s"Cost: ${System.currentTimeMillis() - start}, size: ${results.size}")
+    println(s)
+  }
+
+
+  test("latestVersion") {
+    println(MavenSearchClient.latestVersion("org.scala-lang", "scala-reflect"))
   }
 
   test("To parameters") {
